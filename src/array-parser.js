09190785fa047d098388b6b101e412745ef6b7bb
YanagiEiichi <admin@web-tinker.com>
2015-05-05 21:46:06 +0800

    Use 'parsing loop' for array-parser

--- src/array-parser.js
+++ src/array-parser.js
@@ -6,38 +6,52 @@ var ArrayParser = function(stream, onupdate) {
 
   var that = Parser.call(this, stream, onupdate, 'Array');
 
-  var parseItem = function() {
-    var index = that.result.length;
-    new ValueParser(stream, function() {
-      that.result[index] = this.result;
-      that.$update('LOADING');
-      if(this.state !== 'COMPLETE') return;
-      stream.read(1, function(data) {
-        switch(data) {
-          case ']':
-            return that.$update('COMPLETE');
-          case ',':
-            return parseItem();
-          default:
-            that.$error('invaild array item');
-        }
-      });
-    });
+  var state = 'ENTITY';
+  var length = 1;
+  var parse = function() {
+    var data;
+    while(data = stream.read(length)) {
+      switch(state) {
+        case 'ENTITY':
+          if(data !== '[') that.$error('array must be wrapped by "["');
+          that.result = [];
+          that.state = 'LOADING';
+          state = 'NEXT';
+          break;
+        case 'NEXT':
+          if(data === ']') {
+            that.$update('COMPLETE');
+            return;
+          } else if(that.result.length === 0) {
+            stream.$index--;
+          } else if(data !== ',') {
+            that.$error('require a comma here');
+          }
+          state = 'ITEM';
+          break;
+        case 'ITEM':
+          stream.$index--;
+          var index = that.result.length;
+          var callback = function() {
+            callback = null;
+          }
+          new ValueParser(stream, function() {
+            that.result[index] = this.result;
+            that.$update('LOADING');
+            if(this.state !== 'COMPLETE') return;
+            if(callback) callback();
+          });
+          state = 'NEXT';
+          if(callback) {
+            callback = parse;
+            return;
+          }
+      }
+    }
+    stream.$reading = parse;
   };
 
-  stream.read(1, function(data) {
-    if(data !== '[') that.$error('array must be wrapped by "["');
-    that.result = [];
-    that.state = 'LOADING';
-    stream.read(1, function(data) {
-      if(data === ']') {
-        that.$update('COMPLETE');
-      } else {
-        stream.$index--;
-        parseItem();
-      }
-    });
-  });
+  parse();
 
 };
 
