8e4813035f6f632f2c08559e75542677c3b7d380
YanagiEiichi <admin@web-tinker.com>
2015-05-05 23:04:32 +0800

    Use 'parsing loop' for object-parser

--- src/object-parser.js
+++ src/object-parser.js
@@ -7,44 +7,72 @@ var ObjectParser = function(stream, onupdate) {
 
   var that = Parser.call(this, stream, onupdate, 'Object');
 
-  var parseItem = function() {
-    new StringParser(stream, function() {
-      if(this.state !== 'COMPLETE') return;
-      var key = this.result;
-      stream.read(1, function(data) {
-        if(data !== ':') that.$error('pair must split by ":"');
-        new ValueParser(stream, function() {
-          that.result[key] = this.result;
-          that.$update('LOADING');
-          if(this.state !== 'COMPLETE') return;
-          stream.read(1, function(data) {
-            switch(data) {
-              case '}':
-                return that.$update('COMPLETE');
-              case ',':
-                return parseItem();
-              default:
-                that.$error('invaild array item');
-            }
+  var state = 'ENTITY';
+  var key;
+  var length = 1;
+  var isFirst = true;
+  var parse = function() {
+    var data;
+    while(data = stream.read(length)) {
+      switch(state) {
+        case 'ENTITY':
+          if(data !== '{') that.$error('object must be wrapped by "{"');
+          that.result = {};
+          that.state = 'LOADING';
+          state = 'NEXT';
+          break;
+        case 'NEXT':
+          if(data === '}') {
+            that.$update('COMPLETE');
+            return;
+          } else if(isFirst) {
+            stream.$index--;
+          } else if(data !== ',') {
+            that.$error('require a comma here');
+          }
+          state = 'KEY';
+          break;
+        case 'KEY':
+          stream.$index--;
+          var callback = function() {
+            callback = null;
+          }
+          new StringParser(stream, function() {
+            that.$update('LOADING');
+            if(this.state !== 'COMPLETE') return;
+            key = this.result;
+            if(callback) callback();
           });
-        });
-      });
-    });
+          state = 'VALUE';
+          if(callback) {
+            callback = parse;
+            return;
+          }
+          break;
+        case 'VALUE':
+          if(data !== ':') that.$error('pair must split by ":"');
+          var callback = function() {
+            callback = null;
+          }
+          isFirst = false;
+          new ValueParser(stream, function() {
+            that.result[key] = this.result;
+            that.$update('LOADING');
+            if(this.state !== 'COMPLETE') return;
+            if(callback) callback();
+          });
+          state = 'NEXT';
+          if(callback) {
+            callback = parse;
+            return;
+          }
+          break;
+      }
+    }
+    stream.$reading = parse;
   };
 
-  stream.read(1, function(data) {
-    if(data !== '{') that.$error('object must be wrapped by "{"');
-    that.result = {};
-    that.state = 'LOADING';
-    stream.read(1, function(data) {
-      if(data === '}') {
-        that.$update('COMPLETE');
-      } else {
-        stream.$index--;
-        parseItem();
-      }
-    });
-  });
+  parse();
 
 };
 
