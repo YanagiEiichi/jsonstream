99500c489c30d8d87e6946df3821f3a20cf5fb56
YanagiEiichi <admin@web-tinker.com>
2015-05-05 20:39:44 +0800

    Use 'parsing loop' for string-parser

--- src/string-parser.js
+++ src/string-parser.js
@@ -5,42 +5,55 @@ var ENTITYMAP = { b: '\b', f: '\b', n: '\n', r: '\r', t: '\t', '"': '"', '\\': '
 var StringParser = function(stream, onupdate) {
   var that = Parser.call(this, stream, onupdate, 'String');
 
-  var parseEntity = function(data) {
-    if(data === 'u') {
-      stream.read(4, function(data) {
-        update(String.fromCharCode(parseInt(data, 16)));
-        stream.read(1, parseChar);
-      });
-    } else {
-      var char = ENTITYMAP[data];
-      if(!char) that.$error('invalid entity "\\' + data + '"');
-      update(char);
-      stream.read(1, parseChar);
-    }
-  };
-
-  var parseChar = function(data) {
-    if(data === '\\') {
-      stream.read(1, parseEntity);
-    } else if(data === '"') {
-      that.$update('COMPLETE');
-    } else {
-      update(data);
-      stream.read(1, parseChar);
-    }
-  };
-
   var update = function(data) {
     that.result += data;
     that.$update('LOADING');
   };
 
-  stream.read(1, function(data) {
-    if(data !== '"') that.$error('string must be wrapped by double quote');
-    that.result = '';
-    that.$update('LOADING');
-    stream.read(1, parseChar);
-  });
+  var state = 'ENTITY';
+  var length = 1;
+  var parse = function() {
+    var data;
+    while(data = stream.read(length)) {
+      switch(state) {
+        case 'ENTITY':
+          if(data !== '"') that.$error('string must be wrapped by double quote');
+          that.result = '';
+          that.$update('LOADING');
+          state = 'CHAR';
+          break;
+        case 'CHAR':
+          if(data === '\\') {
+            state = 'BACKSLASH';
+          } else if(data === '"') {
+            that.$update('COMPLETE');
+            return;
+          } else {
+            update(data);
+          }
+          break;
+        case 'BACKSLASH':
+          if(data === 'u') {
+            state = 'UNICODE';
+            length = 4;
+          } else {
+            var char = ENTITYMAP[data];
+            if(!char) that.$error('invalid entity "\\' + data + '"');
+            update(char);
+            state = 'CHAR';
+          }
+          break;
+        case 'UNICODE':
+          update(String.fromCharCode(parseInt(data, 16)));
+          state = 'CHAR';
+          length = 1;
+          break;
+      }
+    }
+    stream.$reading = parse;
+  };
+  
+  parse();
 
 };
 
