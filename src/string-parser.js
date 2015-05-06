dbb39e91b837b157b0d6f222755533b457b4b9e2
YanagiEiichi <admin@web-tinker.com>
2015-05-06 17:03:45 +0800

    Update basic test

--- src/string-parser.js
+++ src/string-parser.js
@@ -1,7 +1,5 @@
 var Parser = require('./parser.js');
 
-var ENTITYMAP = { b: '\b', f: '\b', n: '\n', r: '\r', t: '\t', '"': '"', '\\': '\\', '/': '/' };
-
 var StringParser = function(stream, onupdate) {
   var that = Parser.call(this, stream, onupdate, 'String');
 
